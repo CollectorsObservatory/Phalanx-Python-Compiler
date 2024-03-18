2ed1828484b76c719f2d8c2bb29c53d778557257
Fares Majdoub <126903635+CollectorsObservatory@users.noreply.github.com>
2024-03-17 21:27:34 -0400

    Update phalanxLex.py

--- phalanxLex.py
+++ phalanxLex.py
@@ -1,57 +1,74 @@
-def tokenize(expression) : 
+def tokenize(expression):
     tokens = [] 
-    number = ''
-    keywords = ["if", "else", "elif", "while", "for", "def", "true", "false", "return"]
-    
-    
+    current = ''
+    keywords = ["if", "else", "elif", "while", "for", "def", "true", "false", "return", "print", "and", "or"]
+    op_map = {
+        '+': "ADDITION",
+        '-': "SUBTRACTION",
+        '*': "MULTIPLICATION",
+        '/': "DIVISION",
+        '<': "LESS",
+        '>': "GREAT",
+        '=': "EQUAL",
+        '(': "LEFT BRACKET",
+        ')': "RIGHT BRACKET",
+        '<=': "LESSEQUAL",
+        '>=': "GREATEQUAL",
+        '!=': "NOTEQUAL",
+        '==': "EQUALITY",
+    }
 
-
-    
-    for char in expression :
-        if char.isdigit() or char=='.':
-            number += char
-        
-        elif char in '+-*/' : 
-            if number: 
-                if '.' in number:
-                    tokens.append(("FLOAT" , float(number)))
-                else :
-                    tokens.append(("INTEGER" , int(number)))
-                number = ''
-            
-            if char == '+' :
-                tokens.append(("ADDITION OP", char))
-            elif char == '-' :
-                tokens.append(("SUBSTRACTION OP", char))
-            elif char == '*' :
-                tokens.append(("MULTIPLICATION OP", char))
-            elif char == '/' :
-                tokens.append(("DIVISION OP", char))
-            elif char == '**' :
-                tokens.append(("POWER OP", char))
-                
-    if number:
-        if '.' in number: 
-               tokens.append(("FLOAT" , float(number)))
-        else :
-            tokens.append(("INTEGER" , int(number)))
-            number = ''
+    i = 0
+    while i < len(expression):
+        char = expression[i]
+        if char.isalpha():  
+            current += char
+            while i + 1 < len(expression) and (expression[i + 1].isalpha() or expression[i + 1].isdigit() or expression[i + 1] == '_'):
+                i += 1
+                current += expression[i]
+            if current in keywords:
+                tokens.append(("KEYWORD", current))
+            else:
+                tokens.append(("ID", current))
+            current = ''
+        elif char.isdigit() or char == '.':  
+            current += char
+            while i + 1 < len(expression) and (expression[i + 1].isdigit() or expression[i + 1] == '.'):
+                i += 1
+                current += expression[i]
+            if '.' in current:
+                tokens.append(("FLOAT", float(current)))
+            else:
+                tokens.append(("INTEGER", int(current)))
+            current = ''
+        else:  
+            if i + 1 < len(expression) and char + expression[i + 1] in op_map:  
+                tokens.append((op_map[char + expression[i + 1]] + " OP", char + expression[i + 1]))
+                i += 1
+            elif char in op_map: 
+                tokens.append((op_map[char] + " OP", char))
+        i += 1
     
     return tokens
 
 
-expression = "1.2 + 1.3 + 9 - 4"
-expression2 = "while"
-tokens = tokenize(expression2)
 
-for token in tokens: 
+
+
+def read_and_tokenize(file_path):
+
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+    tokens = tokenize(file_content)
+    
+    return tokens
+
+
+file_path = 'testing.txt'
+
+
+tokens = read_and_tokenize(file_path)
+
+for token in tokens:
     print(token)
-                
-               
-                  
-               
-                  
-                
-                    
-        
-                
+
